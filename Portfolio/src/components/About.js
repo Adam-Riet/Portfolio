bb03a7ea70ede4b536320c3f60493c4d24c18f33
riet <adamriet@gmail.com>
2023-08-02 16:54:49 -0600

    updated about me page

--- Portfolio/src/components/About.js
+++ Portfolio/src/components/About.js
@@ -4,26 +4,28 @@ import '../styles/styles.css';
 
 function About() {
     return (
-        <div className="abcontainer">
-            <h2>About Me</h2> 
-            <div className="abimage">
-                <img src={myImage} alt="About me" /> 
+        <div className="main-container">
+            <div className="left-content">
+                {/* Your left-side content will go here */}
             </div>
+            <div className="abcontainer">
+                <div className="abparagraph">
+                    <p>Recently, I completed a Full Stack Web Development Bootcamp at the University of Utah. Leveraging a decade of sales experience, I am excited to transition into tech, where my expertise in communication, relationship-building, and problem-solving can shine.</p>
+                </div>
 
-            <div className="abparagraph">
-                <p>With over a decade of experience in sales, I have developed a deep understanding of the importance of effective communication, relationship-building, and creative problem-solving. As a lifelong technology enthusiast, I am now channeling my passion into learning programming and expanding my skill set in the ever-evolving world of technology.</p>
-            </div>
-
-            <div className="abparagraph">
-                <p>Driven by my innate curiosity, I am continually exploring new ways to innovate and apply my expertise to real-world challenges. In my spare time, I enjoy immersing myself in the gaming world, where I sharpen my strategic thinking and problem-solving skills. Outside of work and gaming, I cherish the moments spent with friends, sharing experiences and creating lasting memories.</p>
-            </div>
-
-            <div className="abparagraph">
-                <p>With a foundation in sales and a newfound passion for programming, I am excited to embrace new opportunities, learn from diverse perspectives, and contribute to impactful projects in the technology sector.</p>
+                <div className="abparagraph">
+                    <p>My Github and LinkedIn profiles are linked below. Please check out my portfolio for examples of my work. I look forward to connecting with you!
+                    </p>
+                </div>
+                <div className="abimage">
+                    <img src={myImage} alt="About me" /> 
+                </div>
             </div>
         </div>
     );
 }
+
+
 
 
 export default About;
